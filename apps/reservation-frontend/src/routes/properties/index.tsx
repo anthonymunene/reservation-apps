2f731f0c4cf748feeea68f0c6484a99b0c3c1bfc
anthonymunene <munene.anthony@gmail.com>
2022-10-16 18:55:55 +0100

    feat: monorepo: adds frontend app

---
+++ apps/reservation-frontend/src/routes/properties/index.tsx
@@ -0,0 +1,94 @@
+import { component$, Resource } from "@builder.io/qwik";
+import { RequestHandler, useEndpoint } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  const propertyData = useEndpoint<typeof onGet>();
+
+  return (
+    <div>
+      <Resource
+        value={propertyData}
+        onResolved={(propertys) => {
+          return (
+            <>
+              <ul>
+                {propertys.data.map((property) => (
+                  <li>
+                    <div>
+                      <p>{property.id}</p>
+                    </div>
+                    <div>
+                      <p>{property.title}</p>
+                    </div>
+                    <div>
+                      <p>{property.description} </p>
+                    </div>
+                    <div>
+                      <p>{property.city}</p>
+                    </div>
+                    <div>
+                      <p>{property.country}</p>
+                    </div>
+                    <div>
+                      <p>{property.bedrooms}</p>
+                    </div>
+                    <div>
+                      <p>{property.beds}</p>
+                    </div>
+                    <div>
+                      <p>{property.baths}</p>
+                    </div>
+                    <div>
+                      <ul>
+                        {property.amenities.map((amenity) => (
+                          <li>
+                            <p>{amenity}</p>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                    <div>
+                      <p>{property.bedrooms}</p>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </>
+          );
+        }}
+      />
+    </div>
+  );
+});
+
+export const onGet: RequestHandler<EndpointData> = async () => {
+  try {
+    const properties = await fetch("http://localhost:3030/properties").then(
+      (response) => response.json()
+    );
+    return properties;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+interface property {
+  id: number;
+  title: string;
+  description: string;
+  city: string;
+  country: string;
+  bedrooms: number;
+  beds: number;
+  baths: number;
+  entirePlace: boolean;
+  defaultImage: string;
+  amenities: Array<string>;
+}
+
+interface EndpointData {
+  total: number;
+  skip: number;
+  limit: number;
+  data: Array<property>;
+}
---
+++ apps/reservation-frontend/src/routes/users/index.tsx
@@ -0,0 +1,68 @@
+import { component$, Resource } from "@builder.io/qwik";
+import { RequestHandler, useEndpoint } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  const userData = useEndpoint<typeof onGet>();
+
+  return (
+    <div>
+      <Resource
+        value={userData}
+        onResolved={(users) => {
+          return (
+            <>
+              <ul>
+                {users.data.map((user) => (
+                  <li>
+                    <p>{user.id}</p>
+                    <p>{user.firstName}</p>
+                    <p>{user.lastName} </p>
+                    <p>{user.profile.bio}</p>
+                    <p>{user.profile.profilePic}</p>
+                    <p>{user.profile.superHost}</p>
+                  </li>
+                ))}
+              </ul>
+            </>
+          );
+        }}
+      />
+    </div>
+  );
+});
+
+export const onGet: RequestHandler<EndpointData> = async () => {
+  try {
+    const users = await fetch('http://localhost:3030/users').then(response => response.json())
+    return users
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+interface UserProperties {
+  [index: number]: { id: number };
+}
+
+interface UserProfile {
+  id: number;
+  bio: string;
+  profilePic: string;
+  accountId: string;
+  superHost: boolean;
+}
+interface User {
+  id: number;
+  email: string;
+  firstName: string;
+  lastName: string;
+  properties: UserProperties;
+  profile: UserProfile;
+}
+
+interface EndpointData {
+  total: number;
+  skip: number;
+  limit: number;
+  data: Array<User>;
+}
