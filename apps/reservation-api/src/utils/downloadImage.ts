fea6ab7cec7c0d6c4f4e08ba8fda5d766e0029d0
anthonymunene <munene.anthony@gmail.com>
2022-11-12 21:05:52 +0000

    feat: (api):  seeding refactor adds image hashing adds types

--- apps/reservation-api/src/utils/downloadImage.ts
+++ apps/reservation-api/src/utils/downloadImage.ts
@@ -1,16 +1,17 @@
-import { promises } from 'fs';
 
-interface ImageOptions {
-  path: string,
-  category: string
-}
-export const downloadImage = async ({ path, category }:ImageOptions) => {
+
+
+
+export const downloadImage = async (category: string) => {
   const IMAGE_URL = process.env.IMAGE_URL;
-  const response = await fetch(`${IMAGE_URL}/${category}?w=640&h=480`);
-  const blob = await response.blob();
-  const arrayBuffer = await blob.arrayBuffer();
-  const buffer = Buffer.from(arrayBuffer);
-  await promises.writeFile(path, buffer);
+  const response = await fetch(`${IMAGE_URL}/${category}?w=640&h=480`).then(async response => {
+    const blob = await response.blob();
+    const arrayBuffer = await blob.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    return buffer;
+  }).catch(error => {
+    return (error as Error).message;
+  });
 
-  return path;
+  return response;
 };
