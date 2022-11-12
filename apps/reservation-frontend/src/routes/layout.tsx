e53d18245a529401c9f0aab08d339f81ca2c7932
anthonymunene <munene.anthony@gmail.com>
2022-11-12 21:13:07 +0000

    feat: (reservation-frontend):  just messing about

--- apps/reservation-frontend/src/routes/layout.tsx
+++ apps/reservation-frontend/src/routes/layout.tsx
@@ -4,7 +4,7 @@ import Header from '../components/header/header';
 
 export default component$(() => {
   return (
-    <div>
+    <div class="container mx-auto sm:px-6 lg:px-8">
       <Header />
       <main>
         <Slot />
