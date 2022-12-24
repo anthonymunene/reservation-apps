5108138d09964a268e30c6360abe317e7cf14028
anthonymunene <munene.anthony@gmail.com>
2022-12-24 00:47:49 +0000

    feat: (reservation-api): enable multipart form submission

--- apps/reservation-api/src/services/users/users.service.ts
+++ apps/reservation-api/src/services/users/users.service.ts
@@ -1,8 +1,10 @@
 import { Application } from '../../declarations';
+import multer from 'multer';
 
-// Initializes the `users` service on path `/y`
 import { Users } from './users.class';
 import hooks from './users.hooks';
+
+const formDataMiddleware = multer();
 
 export default function (app: Application) {
   const options = {
@@ -13,7 +15,12 @@ export default function (app: Application) {
   };
 
   // Initialize our service with any options it requires
-  app.use('/users', new Users(options, app));
+  app.use('/users', formDataMiddleware.single('profilepic'), function (req , res, next) {
+    req.feathers.files = req.files;
+    next();
+  },
+  new Users(options, app)
+  );
 
   // Get our initialized service so that we can register hooks
   const service = app.service('users');
