c3c152af95a8f3ca62a9fd1b077df7abd49b3452
anthonymunene <munene.anthony@gmail.com>
2023-05-04 16:19:11 +0100

    feat: (frontend-qwik) adds form components

---
+++ apps/reservation-frontend/src/components/FormHeader.tsx
@@ -0,0 +1,35 @@
+import { Fragment } from '@builder.io/qwik';
+import { type ActionStore, Form } from '@builder.io/qwik-city';
+import { reset, type FormStore } from '@modular-forms/qwik';
+import { ActionButton } from './ActionButton';
+
+type FormHeaderProps = {
+  of: FormStore<any, any>;
+  heading: string;
+  resetAction?: ActionStore<void, Record<string, any>>;
+  form?: string;
+};
+
+/**
+ * Form header with heading and buttons to reset and submit the form.
+ */
+export function FormHeader({ of: formStore, heading, resetAction, form }: FormHeaderProps) {
+  const ResetForm = resetAction ? Form : Fragment;
+  return (
+    <header class="flex items-center justify-between px-8 lg:px-10">
+      <h1 class="text-2xl text-slate-900 dark:text-slate-200 md:text-3xl lg:text-4xl">{heading}</h1>
+      <div class="hidden lg:flex lg:space-x-8">
+        <ResetForm action={resetAction!}>
+          <ActionButton
+            variant="secondary"
+            label="Reset"
+            type={resetAction ? 'submit' : 'button'}
+            preventdefault:click
+            onClick$={() => reset(formStore)}
+          />
+        </ResetForm>
+        <ActionButton variant="primary" label="Submit" type="submit" form={form} />
+      </div>
+    </header>
+  );
+}
---
+++ apps/reservation-frontend/src/components/UnstyledButton.tsx
@@ -0,0 +1,80 @@
+import { $, component$, type PropFunction, Slot, useSignal } from '@builder.io/qwik';
+import { Link } from '@builder.io/qwik-city';
+import clsx from 'clsx';
+import { Spinner } from './Spinner';
+
+type LinkProps = {
+  type: 'link';
+  href: string;
+  download?: boolean | string;
+  target?: '_blank';
+};
+
+type ButtonProps = {
+  type: 'button' | 'reset' | 'submit';
+  'preventdefault:click'?: boolean;
+  onClick$?: PropFunction<() => unknown>;
+  loading?: boolean;
+  form?: string;
+};
+
+export type DefaultButtonProps = LinkProps | ButtonProps;
+
+type UnstyledButtonProps = DefaultButtonProps & {
+  class?: string;
+  'aria-label'?: string;
+};
+
+/**
+ * Basic button component that contains important functionality and is used to
+ * build more complex components on top of it.
+ */
+export const UnstyledButton = component$((props: UnstyledButtonProps) => {
+  // Use loading signal
+  const loading = useSignal(false);
+
+  return (
+    <>
+      {/* Link button */}
+      {props.type === 'link' && (
+        <Link {...props} rel={props.target === '_blank' ? 'noreferrer' : undefined}>
+          <Slot />
+        </Link>
+      )}
+
+      {/* Normal button */}
+      {props.type !== 'link' && (
+        <button
+          {...props}
+          // disabled={loading.value || props.loading}
+          // Start and stop loading if function is async
+          onClick$={
+            props.onClick$ &&
+            $(async () => {
+              loading.value = true;
+              await props.onClick$!();
+              loading.value = false;
+            })
+          }
+        >
+          <div
+            class={clsx(
+              'transition-[opacity,transform,visibility] duration-200',
+              loading.value || props.loading ? 'invisible translate-x-5 opacity-0' : 'visible delay-300'
+            )}
+          >
+            <Slot />
+          </div>
+          <div
+            class={clsx(
+              'absolute duration-200',
+              loading.value || props.loading ? 'visible delay-300' : 'invisible -translate-x-5 opacity-0'
+            )}
+          >
+            <Spinner />
+          </div>
+        </button>
+      )}
+    </>
+  );
+});
