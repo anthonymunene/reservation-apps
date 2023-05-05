607db27ba5bed912e8ecf1b2cb9a1b5dce1521b4
anthonymunene <munene.anthony@gmail.com>
2023-05-05 09:01:18 +0100

    feat: (frontend-qwik) adds tailwind styling

---
+++ apps/reservation-frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+import  defaultTheme from  'tailwindcss/defaultTheme';
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  container: {
+    center: true,
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
