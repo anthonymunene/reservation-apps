6b331d5d1c87d0859b696b826cfa65363f69b7e3
anthonymunene <munene.anthony@gmail.com>
2023-12-06 01:39:55 +0000

    chore: (api) tidy up

--- apps/api/src/services/uploads/uploads.ts
+++ apps/api/src/services/uploads/uploads.ts
@@ -8,18 +8,7 @@ import { Service } from '../../utils/s3/lib';
 
 // A configure function that registers the service and its hooks via `app.configure`
 export const uploads = async (app: Application) => {
-  const S3Options = {
-    s3Client: {
-      credentials: {
-        accessKeyId: 'AKIAXXFURR4TZAKVDXEJ',
-        secretAccessKey: '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',
-      },
-      endpoint: process.env.S3_ENDPOINT,
-      region: 'eu-west-2',
-      signatureVersion: 'v4',
-    },
-    bucket: `dl-reservation-app-assets`,
-  };
+  const S3Options = {};
   // const UploadsService = S3.Service;
   // Register our service on the Feathers application
   app.use(uploadsPath, new Service(S3Options), {
