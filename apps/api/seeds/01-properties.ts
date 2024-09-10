4d1e38b90eb1d69da5beb94511b5da35979fac33
anthonymunene <munene.anthony@gmail.com>
2024-09-10 14:27:38 +0300

    chore: (api) use commonjs for seeding data

--- apps/api/seeds/01-properties.ts
+++ apps/api/seeds/01-properties.ts
@@ -2,30 +2,30 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 
-import { faker } from '@faker-js/faker';
-import { Knex } from 'knex';
-import { PROPERTY } from '../src/utils/variables';
-import { randomiseArray, randomiseInt } from '../src/utils/randomise';
+const { faker } = require("@faker-js/faker")
+import { Knex } from "knex"
+import { PROPERTY } from "../src/utils/variables"
+import { randomiseArray } from "../src/utils/randomise"
 // import { createIfNotExist, seedImages, clearImageFolder } from '../utils/seedImages';
-import { Properties } from '../src/services/properties/properties.schema';
-import { Amenity, PropertyType } from '../src/types';
-import { randomUUID } from 'crypto';
+import { Properties } from "../src/services/properties/properties.schema"
+import { Amenity, PropertyType } from "../src/types"
+import { randomUUID } from "crypto"
 
 //TYPES-TO-COLLATE:
-type PropertyId = Pick<Properties, 'id'>;
-type PropertyTypeData = Pick<PropertyType, 'id' | 'name'>;
+type PropertyId = Pick<Properties, "id">
+type PropertyTypeData = Pick<PropertyType, "id" | "name">
 
-type AmenityData = Pick<Amenity, 'id' | 'name'>;
+type AmenityData = Pick<Amenity, "id" | "name">
 
-export const getAmenitiesById = async (dbClient: Knex): Pick<AmenityData, 'id'>[] => {
-  const amenities: AmenityData[] = await dbClient.select('id', 'name').from('Amenity');
-  return amenities;
-};
+export const getAmenitiesById = async (dbClient: Knex): Pick<AmenityData, "id">[] => {
+  const amenities: AmenityData[] = await dbClient.select("id", "name").from("Amenity")
+  return amenities
+}
 
 export const getAllPropertyTypes = async (dbClient: Knex): PropertyTypeData[] => {
-  const propertyTypes: PropertyTypeData[] = await dbClient.select('id', 'name').from('PropertyType');
-  return propertyTypes;
-};
+  const propertyTypes: PropertyTypeData[] = await dbClient.select("id", "name").from("PropertyType")
+  return propertyTypes
+}
 
 // export const getPropertyTypeById = (dbClient: PrismaClient, id: number) => {
 
@@ -44,21 +44,21 @@ export const getAllPropertyTypes = async (dbClient: Knex): PropertyTypeData[] =>
 //   faker.helpers.arrayElements(amenities, 2);
 
 export const createProperties = async (dbClient: Knex): Promise<void> => {
-  const amenities = await getAmenitiesById(dbClient);
-  const propertyTypes = await getAllPropertyTypes(dbClient);
+  const amenities = await getAmenitiesById(dbClient)
+  const propertyTypes = await getAllPropertyTypes(dbClient)
 
   const properties = Array.from({ length: 10 }).map(() => {
     return {
       id: randomUUID(),
       description: faker.lorem.sentences(),
-      city: faker.address.city(),
-      countryCode: faker.address.countryCode(),
+      city: faker.location.city(),
+      countryCode: faker.location.countryCode(),
       bedrooms: faker.helpers.arrayElement([1, 2, 3]),
       beds: faker.helpers.arrayElement([1, 2, 3]),
       images: {},
       // entirePlace: faker.helpers.arrayElement([true, false])
-    };
-  });
+    }
+  })
 
   // const connectAmenities = (data: []) =>
   //   data.map((item) => ({ amenity: { connect: item } }));
@@ -68,68 +68,69 @@ export const createProperties = async (dbClient: Knex): Promise<void> => {
   //   .then((deletedFiles) => console.log('done! deleted files:', deletedFiles));
   const addAmenities = (amenities: AmenityData[], property: PropertyId): void => {
     amenities.map(async (amenity: AmenityData) => {
-      await dbClient('PropertyAmenity').insert({
+      await dbClient("PropertyAmenity").insert({
         id: randomUUID(),
         amenityId: amenity.id,
         propertyId: property.id,
-      });
-    });
-  };
+      })
+    })
+  }
   await Promise.all(
     properties.map(async property => {
-      const assignedPropertyType: PropertyTypeData = randomiseArray(propertyTypes);
-      const assignedAmenities: AmenityData[] = randomiseArray(amenities, 3);
-      const title = `${faker.word.adjective(7)} ${assignedPropertyType.name}`;
+      const assignedPropertyType: PropertyTypeData = randomiseArray(propertyTypes)
+      const assignedAmenities: AmenityData[] = randomiseArray(amenities, 3)
+      const title = `${faker.word.adjective(7)} ${assignedPropertyType.name}`
 
       // createIfNotExist(propertiesImageDir);
       // const propertyImage = await seedImages('house', {
       //   dir: propertiesImageDir,
       //   property: { title }
       // });
-      const propertyId: PropertyId[] = await dbClient('Property').insert(
+      const propertyId: PropertyId[] = await dbClient("Property").insert(
         {
           id: randomUUID(),
           title: title,
           ...property,
           propertyTypeId: assignedPropertyType.id,
         },
-        ['id']
-      );
+        ["id"]
+      )
 
-      addAmenities(assignedAmenities, propertyId[0]);
+      addAmenities(assignedAmenities, propertyId[0])
     })
-  );
-};
+  )
+}
 
 export const createPropertyTypes = async (dbClient: Knex): Promise<void> => {
-  const { PROPERTY_TYPES } = PROPERTY;
+  console.log("CREATING PROPERTY TYPES.....")
+  const { PROPERTY_TYPES } = PROPERTY
   const propertyTypeData = PROPERTY_TYPES.map(
     (propertyType): PropertyTypeData => ({
       id: randomUUID(),
       name: propertyType,
     })
-  );
+  )
 
-  const propertyTypes = await dbClient('PropertyType').whereIn('name', PROPERTY_TYPES);
-  return propertyTypes.length ? propertyTypes : await dbClient.insert(propertyTypeData, ['id']).into('PropertyType');
-};
+  const propertyTypes = await dbClient("PropertyType").whereIn("name", PROPERTY_TYPES)
+  return propertyTypes.length ? propertyTypes : await dbClient.insert(propertyTypeData, ["id"]).into("PropertyType")
+}
 
 export const createAmenities = async (dbClient: Knex): Promise<void> => {
-  const { AMENITIES } = PROPERTY;
+  console.log("CREATING AMENITIES.....")
+  const { AMENITIES } = PROPERTY
   const amenityData = AMENITIES.map(
     (amenity): AmenityData => ({
       id: randomUUID(),
       name: amenity,
     })
-  );
-  const amenities = await dbClient('Amenity').whereIn('name', AMENITIES);
+  )
+  const amenities = await dbClient("Amenity").whereIn("name", AMENITIES)
 
-  return amenities.length ? amenities : await dbClient.insert(amenityData, ['id']).into('Amenity');
-};
-
+  return amenities.length ? amenities : await dbClient.insert(amenityData, ["id"]).into("Amenity")
+}
 
 export async function seed(knex: Knex): Promise<void> {
-  await createAmenities(knex);
-  await createPropertyTypes(knex);
-  await createProperties(knex);
+  await createAmenities(knex)
+  await createPropertyTypes(knex)
+  await createProperties(knex)
 }
--- apps/api/seeds/02-users.ts
+++ apps/api/seeds/02-users.ts
@@ -1,47 +1,47 @@
 // @ts-nocheck
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-import { Knex } from 'knex';
-import { faker } from '@faker-js/faker';
-import { imageSeeder } from './utils/seedImages';
-import { Users, UsersData } from '../src/services/users/users.schema';
-import { Properties } from '../src/services/properties/properties.schema';
-import { Profiles } from '../src/services/profiles/profiles.schema';
-import { randomUUID } from 'crypto';
+import { Knex } from "knex"
+const { faker } = require("@faker-js/faker")
+import { imageSeeder } from "./utils/seedImages"
+import { Users, UsersData } from "../src/services/users/users.schema"
+import { Properties } from "../src/services/properties/properties.schema"
+import { Profiles } from "../src/services/profiles/profiles.schema"
+import { randomUUID } from "crypto"
 
-import { randomiseInt } from '../src/utils/randomise';
-import reviews from './data/reviews.json';
-import { uploadToImageStorage } from '../src/utils/imageStorageProvider';
+import { randomiseInt } from "../src/utils/randomise"
+import reviews from "./data/reviews.json"
+import { uploadToImageStorage } from "../src/utils/imageStorageProvider"
 // //TODO: move to config
 const userAccounts: Array<UsersData> = Array.from({ length: 10 }).map(() => {
   return {
     id: randomUUID(),
     email: faker.internet.email(),
     password: faker.internet.password(),
-  };
-});
+  }
+})
 
 //TYPES-TO-COLLATE:
-type PropertyId = Pick<Properties, 'id'>;
-type UserId = Pick<Users, 'id'>;
+type PropertyId = Pick<Properties, "id">
+type UserId = Pick<Users, "id">
 
-const getMultipleRandomisedPropertyIds = (propertyId: PropertyId[]) => faker.helpers.arrayElements(propertyId, 2);
+const getMultipleRandomisedPropertyIds = (propertyId: PropertyId[]) => faker.helpers.arrayElements(propertyId, 2)
 
 const getAllPropertiesWithoutOwner = async (knex: Knex) => {
-  const propertiesById: PropertyId[] = await knex.select('id').from('Property').whereNull('host');
-  console.log(`Properties without owners by Id ${JSON.stringify(propertiesById)}`);
-  return propertiesById;
-};
-type ProfileData = Pick<Profiles, 'id' | 'userId' | 'firstName' | 'surname' | 'bio' | 'defaultPic'>;
+  const propertiesById: PropertyId[] = await knex.select("id").from("Property").whereNull("host")
+  console.log(`Properties without owners by Id ${JSON.stringify(propertiesById)}`)
+  return propertiesById
+}
+type ProfileData = Pick<Profiles, "id" | "userId" | "firstName" | "surname" | "bio" | "defaultPic">
 
 const createProfile = async (knex: Knex, user: UserId) => {
-  const firstName = faker.person.firstName();
-  const surname = faker.person.lastName();
-  const seedImages = await imageSeeder();
+  const firstName = faker.person.firstName()
+  const surname = faker.person.lastName()
+  const seedImages = await imageSeeder()
   const imageURL = await seedImages({
-    type: 'users',
+    type: "users",
     name: `${firstName}-${surname}`,
-  });
-  const uploadResult = await uploadToImageStorage(imageURL);
+  })
+  const uploadResult = await uploadToImageStorage(imageURL)
   const profile: ProfileData = {
     id: randomUUID(),
     firstName,
@@ -49,32 +49,33 @@ const createProfile = async (knex: Knex, user: UserId) => {
     bio: faker.lorem.sentences(),
     userId: user.id,
     defaultPic: { url: uploadResult },
-  };
+  }
 
-  const userProfile: Pick<ProfileData, 'id' | 'firstName' | 'surname'>[] = await knex
+  const userProfile: Pick<ProfileData, "id" | "firstName" | "surname">[] = await knex
     .insert(profile)
-    .into('Profile')
-    .returning(['id', 'firstName', 'surname']);
+    .into("Profile")
+    .returning(["id", "firstName", "surname"])
 
-  console.log(`Returned profileId ${JSON.stringify(userProfile[0].id)}`);
-  return userProfile[0];
-};
+  console.log(`Returned profileId ${JSON.stringify(userProfile[0].id)}`)
+  return userProfile[0]
+}
 
 const createUsers = async (knex: Knex) => {
+  console.log(`Creating users...`)
   // createIfNotExist(seedUserImageDir);
   // await clearImageFolder(`${seedUserImageDir}/*.png`)
   //   .then((deletedFiles) => console.log('done! deleted files:', deletedFiles))
   const ownProperty = async (properties: PropertyId[], user: UserId) => {
     properties.map(async property => {
-      const { id } = user;
-      console.log(`User: ${user.id} \n Properties to own: \n ${JSON.stringify(properties)} \n\n`);
-      await knex('Property')
-        .where('id', (property as PropertyId)['id'])
+      const { id } = user
+      console.log(`User: ${user.id} \n Properties to own: \n ${JSON.stringify(properties)} \n\n`)
+      await knex("Property")
+        .where("id", (property as PropertyId)["id"])
         .update({
           host: id,
-        });
-    });
-  };
+        })
+    })
+  }
   //   .catch(error => console.log(`something went wrong deleting files:\n${error}`));
 
   for (let index = 0; index < userAccounts.length; index++) {
@@ -82,33 +83,34 @@ const createUsers = async (knex: Knex) => {
       // interface propertiesToOwnType {
       //   [key: string]: string;
       // }
-      return getMultipleRandomisedPropertyIds(id);
+      return getMultipleRandomisedPropertyIds(id)
 
       // const userIdSchema = Type.Array(Type.Object({UserId: Type.Number()}));
       // type UserId = Static<typeof userIdSchema>
-    });
+    })
 
-    const [user]: UserId[] = await knex.insert(userAccounts[index], ['id']).into('User');
+    const [user]: UserId[] = await knex.insert(userAccounts[index], ["id"]).into("User")
 
-    const { id } = await createProfile(knex, user);
+    const { id } = await createProfile(knex, user)
 
-    await knex('User').where({ id: user.id }).update('profileId', id);
+    await knex("User").where({ id: user.id }).update("profileId", id)
     if (freeProperties.length) {
-      await ownProperty(freeProperties, user);
+      await ownProperty(freeProperties, user)
     } else {
-      console.log(`not creating profiles for User ${user.id}} `);
+      console.log(`not creating profiles for User ${user.id}} `)
     }
   }
-};
+}
 
 const createReviews = async (dbClient: Knex): Promise<void> => {
-  const users: Users[] = await dbClient.select('id').from('User');
-  const properties = await dbClient.select('id').from('Property');
-  const allReviews = [...reviews.positive, ...reviews.negative, ...reviews.mixed];
+  console.log(`Creating reviews...`)
+  const users: Users[] = await dbClient.select("id").from("User")
+  const properties = await dbClient.select("id").from("Property")
+  const allReviews = [...reviews.positive, ...reviews.negative, ...reviews.mixed]
   for (let index = 0; index < properties.length; index++) {
-    const { id }: UserId = users[randomiseInt(users.length)];
+    const { id }: UserId = users[randomiseInt(users.length)]
 
-    const review = allReviews[randomiseInt(allReviews.length)];
+    const review = allReviews[randomiseInt(allReviews.length)]
     await dbClient
       .insert({
         id: randomUUID(),
@@ -116,13 +118,13 @@ const createReviews = async (dbClient: Knex): Promise<void> => {
         userId: id,
         comment: review,
       })
-      .into('Review');
+      .into("Review")
   }
-};
+}
 
 export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries
   // await knex('User').del();
-  await createUsers(knex);
-  await createReviews(knex);
+  await createUsers(knex)
+  await createReviews(knex)
 }
