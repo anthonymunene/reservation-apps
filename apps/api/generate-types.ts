c8a20f91c341240123fea742eb3abba2046fcf23
anthonymunene <munene.anthony@gmail.com>
2024-09-10 14:56:42 +0300

    chore: (api) updates linting rules

--- apps/api/generate-types.ts
+++ apps/api/generate-types.ts
@@ -1,14 +1,15 @@
-import knex from 'knex';
-import { updateTypes } from 'knex-types';
-import { app } from './src/app';
+import knex from "knex"
+import { updateTypes } from "knex-types"
+import { app } from "./src/app"
 
-const config = app.get('postgresql');
+const config = app.get("postgresql")
 
-const db = knex({ ...config, debug: true });
+const db = knex({ ...config, debug: true })
+// @ts-ignore
 updateTypes(db, {
-  output: './src/types.ts',
-  exclude: ['knex_migrations', 'knex_migrations_lock'],
+  output: "./src/types.ts",
+  exclude: ["knex_migrations", "knex_migrations_lock"],
 }).catch(err => {
-  console.error(err);
-  process.exit(1);
-});
+  console.error(err)
+  process.exit(1)
+})
