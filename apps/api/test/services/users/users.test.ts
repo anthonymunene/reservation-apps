9116ae5657247a6d78799b822d40a8140841baef
anthonymunene <munene.anthony@gmail.com>
2024-09-10 14:32:47 +0300

    chore: (api) updates linting rules

--- apps/api/test/services/users/users.test.ts
+++ apps/api/test/services/users/users.test.ts
@@ -1,22 +1,22 @@
 // For more information about this file see https://dove.feathersjs.com/guides/cli/service.test.html
-import assert from 'assert';
-import { app } from '../../../src/app';
-import { faker } from '@faker-js/faker';
-import { describe } from 'mocha';
+import assert from "assert"
+import { app } from "../../../src/app"
+const { faker } = require("@faker-js/faker")
+import { describe } from "mocha"
 
-describe('users service', () => {
-  it('registered the service', () => {
-    const service = app.service('users');
-    assert.ok(service, 'Registeredddddddd the service');
-  });
-  it('creates a new user', async () => {
-    const email = faker.internet.email();
-    const user = await app.service('users').create({
+describe("users service", () => {
+  it("registered the service", () => {
+    const service = app.service("users")
+    assert.ok(service, "Registeredddddddd the service")
+  })
+  it("creates a new user", async () => {
+    const email = faker.internet.email()
+    const user = await app.service("users").create({
       email: email,
       password: faker.internet.password(),
-    });
-    assert.ok(user, 'Success Created User');
+    })
+    assert.ok(user, "Success Created User")
     //should automatically creates a user profile
-    assert.equal(user.profile.bio, 'insert bio', 'Success Created Profile');
-  });
-});
+    assert.equal(user.profile.bio, "insert bio", "Success Created Profile")
+  })
+})
