47d874a53679a6322978c1e9e485faea15d8a7c5
anthonymunene <munene.anthony@gmail.com>
2024-09-11 15:23:09 +0300

    feat: (api) adds storage configuration

--- apps/api/src/configuration.ts
+++ apps/api/src/configuration.ts
@@ -1,7 +1,7 @@
-import { Type, getValidator, defaultAppConfiguration } from '@feathersjs/typebox';
-import type { Static } from '@feathersjs/typebox';
+import { Type, getValidator, defaultAppConfiguration } from "@feathersjs/typebox"
+import type { Static } from "@feathersjs/typebox"
 
-import { dataValidator } from './validators';
+import { dataValidator } from "./validators"
 
 export const configurationSchema = Type.Intersect([
   defaultAppConfiguration,
@@ -9,9 +9,20 @@ export const configurationSchema = Type.Intersect([
     host: Type.String(),
     port: Type.Number(),
     public: Type.String(),
+    storage: Type.Object({
+      s3Client: Type.Object({
+        accessKeyId: Type.String(),
+        secretAccessKey: Type.String(),
+        endpoint: Type.String(),
+        region: Type.String(),
+        signatureVersion: Type.String(),
+        bucket: Type.String(),
+        prefix: Type.String(),
+      }),
+    }),
   }),
-]);
+])
 
-export type ApplicationConfiguration = Static<typeof configurationSchema>;
+export type ApplicationConfiguration = Static<typeof configurationSchema>
 
-export const configurationValidator = getValidator(configurationSchema, dataValidator);
+export const configurationValidator = getValidator(configurationSchema, dataValidator)
