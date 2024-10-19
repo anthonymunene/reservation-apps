a94913c1930984ab8041ed271f32a94a7c83cf81
anthonymunene <munene.anthony@gmail.com>
2024-10-19 16:33:29 +0100

    feat: (frontend-qwik) update input props

--- apps/reservation-frontend/src/components/form/fileInput/FileInput.tsx
+++ apps/reservation-frontend/src/components/form/fileInput/FileInput.tsx
@@ -1,30 +1,22 @@
-import {
-  component$,
-  type NoSerialize,
-  useSignal,
-  useTask$,
-  type PropFunction,
-  type QwikChangeEvent,
-  type QwikFocusEvent,
-} from '@builder.io/qwik';
-import clsx from 'clsx';
-import { InputError } from '../inputError/InputError';
-import { InputLabel } from '../inputLabel/InputLabel';
+import { component$, type NoSerialize, type QRL, useSignal, useTask$ } from "@builder.io/qwik"
+import clsx from "clsx"
+import { InputError } from "../inputError/InputError"
+import { InputLabel } from "../inputLabel/InputLabel"
 
 type FileInputProps = {
-  ref: PropFunction<(element: Element) => void>;
-  name: string;
-  value: NoSerialize<Blob> | NoSerialize<Blob>[] | NoSerialize<File> | NoSerialize<File>[] | null | undefined;
-  onInput$: PropFunction<(event: Event, element: HTMLInputElement) => void>;
-  onChange$: PropFunction<(event: QwikChangeEvent<HTMLInputElement>, element: HTMLInputElement) => void>;
-  onBlur$: PropFunction<(event: QwikFocusEvent<HTMLInputElement>, element: HTMLInputElement) => void>;
-  accept?: string;
-  required?: boolean;
-  multiple?: boolean;
-  class?: string;
-  label?: string;
-  error?: string;
-};
+  ref: QRL<(element: HTMLInputElement) => void>
+  name: string
+  value: NoSerialize<Blob> | NoSerialize<Blob>[] | NoSerialize<File> | NoSerialize<File>[] | null | undefined
+  onInput$: (event: Event, element: HTMLInputElement) => void
+  onChange$: (event: Event, element: HTMLInputElement) => void
+  onBlur$: (event: Event, element: HTMLInputElement) => void
+  accept?: string
+  required?: boolean
+  multiple?: boolean
+  class?: string
+  label?: string
+  error?: string
+}
 
 /**
  * File input field that users can click or drag files into. Various
@@ -32,27 +24,30 @@ type FileInputProps = {
  * requirements.
  */
 export const FileInput = component$(({ value, label, error, ...props }: FileInputProps) => {
-  const { name, required, multiple } = props;
+  const { name, required, multiple } = props
 
   // Create computed value of selected files
-  const files = useSignal<NoSerialize<Blob>[] | NoSerialize<File>[]>();
+  const files = useSignal<NoSerialize<Blob>[] | NoSerialize<File>[]>()
   useTask$(({ track }) => {
-    track(() => value);
-    files.value = value ? (Array.isArray(value) ? value : [value]) : [];
-  });
+    track(() => value)
+    files.value = value ? (Array.isArray(value) ? value : [value]) : []
+  })
 
   return (
-    <div class={clsx('px-8 lg:px-10', props.class)}>
+    <div class={clsx("px-8 lg:px-10", props.class)}>
       <InputLabel name={name} label={label} required={required} />
       <label
         class={clsx(
-          'relative flex min-h-[96px] w-full items-center justify-center rounded-2xl border-[3px] border-dashed border-slate-200 p-8 text-center focus-within:border-sky-600/50 hover:border-slate-300 dark:border-slate-800 dark:focus-within:border-sky-400/50 dark:hover:border-slate-700 md:min-h-[112px] md:text-lg lg:min-h-[128px] lg:p-10 lg:text-xl',
-          !files.value?.length && 'text-slate-500'
+          "relative flex min-h-[96px] w-full items-center justify-center rounded-2xl border-[3px] border-dashed border-slate-200 p-8 text-center focus-within:border-sky-600/50 hover:border-slate-300 dark:border-slate-800 dark:focus-within:border-sky-400/50 dark:hover:border-slate-700 md:min-h-[112px] md:text-lg lg:min-h-[128px] lg:p-10 lg:text-xl",
+          !files.value?.length && "text-slate-500"
         )}
       >
-        {/* {files.value?.length
-          ? `Selected file${multiple ? 's' : ''}: ${files.value.map(file => file!.name).join(', ')}`
-          : `Click or drag and drop file${multiple ? 's' : ''}`} */}
+        {files.value?.length
+          ? `Selected file${multiple ? "s" : ""}: ${files.value
+              // @ts-ignore
+              .map(file => file!.name)
+              .join(", ")}`
+          : `Click or drag and drop file${multiple ? "s" : ""}`}
         <input
           {...props}
           class="absolute h-full w-full cursor-pointer opacity-0"
@@ -62,7 +57,7 @@ export const FileInput = component$(({ value, label, error, ...props }: FileInpu
           aria-errormessage={`${name}-error`}
         />
       </label>
-      {/* <InputError name={name} error={error} /> */}
+      <InputError name={name} error={error} />
     </div>
-  );
-});
+  )
+})
