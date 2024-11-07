0342a4de22043fa7604ca744beb1f3e20da4d9ef
anthonymunene <munene.anthony@gmail.com>
2024-11-07 18:18:11 +0100

    feat: (api) remove unnecessary column

--- apps/api/src/types.ts
+++ apps/api/src/types.ts
@@ -12,82 +12,80 @@ export enum Table {
 }
 
 export type Tables = {
-  "Amenity": Amenity,
-  "Profile": Profile,
-  "Property": Property,
-  "PropertyAmenity": PropertyAmenity,
-  "PropertyType": PropertyType,
-  "Review": Review,
-  "User": User,
-};
+  Amenity: Amenity
+  Profile: Profile
+  Property: Property
+  PropertyAmenity: PropertyAmenity
+  PropertyType: PropertyType
+  Review: Review
+  User: User
+}
 
 export type Amenity = {
-  id: string;
-  name: string | null;
-  createdAt: Date;
-  updatedAt: Date;
-};
+  id: string
+  name: string | null
+  createdAt: Date
+  updatedAt: Date
+}
 
 export type Profile = {
-  id: string;
-  firstName: string | null;
-  surname: string | null;
-  otherNames: string | null;
-  bio: string | null;
-  isSuperHost: boolean;
-  defaultPic: Record<string, unknown> | null;
-  profilePics: Record<string, unknown> | null;
-  userId: string | null;
-  createdAt: Date;
-  updatedAt: Date;
-};
+  id: string
+  firstName: string | null
+  surname: string | null
+  otherNames: string | null
+  bio: string | null
+  isSuperHost: boolean
+  defaultPic: Record<string, unknown> | null
+  profilePics: Record<string, unknown> | null
+  userId: string | null
+  createdAt: Date
+  updatedAt: Date
+}
 
 export type Property = {
-  id: string;
-  title: string;
-  description: string | null;
-  city: string | null;
-  countryCode: string | null;
-  bedrooms: number | null;
-  beds: number | null;
-  images: unknown[] | null;
-  host: string | null;
-  propertyTypeId: string | null;
-  createdAt: Date;
-  updatedAt: Date;
-};
+  id: string
+  title: string
+  description: string | null
+  city: string | null
+  countryCode: string | null
+  bedrooms: number | null
+  beds: number | null
+  images: unknown[] | null
+  host: string | null
+  propertyTypeId: string | null
+  createdAt: Date
+  updatedAt: Date
+}
 
 export type PropertyAmenity = {
-  id: string;
-  propertyId: string | null;
-  amenityId: string | null;
-  createdAt: Date;
-  updatedAt: Date;
-};
+  id: string
+  propertyId: string | null
+  amenityId: string | null
+  createdAt: Date
+  updatedAt: Date
+}
 
 export type PropertyType = {
-  id: string;
-  name: string;
-  createdAt: Date;
-  updatedAt: Date;
-};
+  id: string
+  name: string
+  createdAt: Date
+  updatedAt: Date
+}
 
 export type Review = {
-  id: string;
-  propertyId: string | null;
-  userId: string | null;
-  comment: string | null;
-  createdAt: Date;
-  updatedAt: Date;
-};
+  id: string
+  propertyId: string | null
+  userId: string | null
+  comment: string | null
+  createdAt: Date
+  updatedAt: Date
+}
 
 export type User = {
-  id: string;
-  email: string;
-  password: string;
-  profileId: string | null;
-  createdAt: Date;
-  updatedAt: Date;
-  lastLogin: Date | null;
-};
-
+  id: string
+  email: string
+  password: string
+  createdAt: Date
+  updatedAt: Date
+  lastLogin: Date | null
+}
