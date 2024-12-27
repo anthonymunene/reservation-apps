a824b21500688d1a3c9596d5f3960fa2939b158d
anthonymunene <munene.anthony@gmail.com>
2024-12-27 11:39:03 +0000

    feat: (api) refactor properties seeding
     makes use of neverthrow to safely handle errors
     minor refactor of constants

--- apps/api/seeds/01-properties.ts
+++ apps/api/seeds/01-properties.ts
@@ -6,23 +6,22 @@ import { faker } from "@faker-js/faker"
 import { Knex } from "knex"
 import { PROPERTY } from "@utils/variables"
 import { randomiseMany, randomiseOne } from "@utils/randomise"
-// import { createIfNotExist, seedImages, clearImageFolder } from '../utils/seedImages';
-import { Properties } from "@services/properties/properties.schema"
-import { Amenity, PropertyType, Table } from "@database-generated-types"
+import { Table } from "@database-generated-types/knex-db"
 import { randomUUID } from "crypto"
-import { generateImages, getMatchingFile, replacePrimaryImageForEntity, uploadToS3 } from "@seeds/utils/shared"
-import { PROPERTIES_IMAGE_DIR } from "@seeds/utils/variables"
+import {
+  generateImages,
+  getMatchingFile,
+  replacePrimaryImageForEntity,
+  uploadToS3,
+} from "@seeds/utils/shared"
+import {
+  PROPERTIES_IMAGE_DIR,
+  TOTAL_AMENITIES_PER_PROPERTY,
+  TOTAL_PROPERTIES,
+  TOTAL_ROOMS,
+} from "@seeds/utils/variables"
 
-interface PropertyId extends Pick<Properties, "id"> {}
-
-interface PropertyTypeData extends Pick<PropertyType, "id" | "name"> {}
-
-interface AmenityData extends Pick<Amenity, "id" | "name"> {}
-
-const ROOMS_RANGE = [1, 2, 3] as const
-const NUM_PROPERTIES = 10
-const NUM_AMENITIES_PER_PROPERTY = 3
-//TYPES-TO-COLLATE:
+import type { AmenityData, PropertyId, PropertyTypeData } from "@seeds/utils/types/properties"
 
 export const getAmenitiesById = async (dbClient: Knex): Promise<AmenityData[]> =>
   await dbClient.select("id", "name").from(Table.Amenity)
@@ -30,7 +29,11 @@ export const getAmenitiesById = async (dbClient: Knex): Promise<AmenityData[]> =
 export const getAllPropertyTypes = async (dbClient: Knex): Promise<PropertyTypeData[]> =>
   await dbClient.select("id", "name").from(Table.PropertyType)
 
-const addAmenities = async (dbClient: Knex, amenities: AmenityData[], property: PropertyId): Promise<void> => {
+const addAmenities = async (
+  dbClient: Knex,
+  amenities: AmenityData[],
+  property: PropertyId
+): Promise<void> => {
   const amenityRecords = amenities.map(amenity => ({
     id: randomUUID(),
     amenityId: amenity.id,
@@ -45,8 +48,8 @@ const generateProperty = () => ({
   description: faker.lorem.sentences(),
   city: faker.location.city(),
   countryCode: faker.location.countryCode(),
-  bedrooms: faker.helpers.arrayElement(ROOMS_RANGE),
-  beds: faker.helpers.arrayElement(ROOMS_RANGE),
+  bedrooms: faker.helpers.arrayElement(TOTAL_ROOMS),
+  beds: faker.helpers.arrayElement(TOTAL_ROOMS),
   images: {},
   // entirePlace: faker.helpers.arrayElement([true, false])
 })
@@ -58,16 +61,21 @@ export const createProperties = async (
   }
 ): Promise<PropertyId[]> => {
   const { getAmenitiesById, getAllPropertyTypes } = dependencies
-  const [amenities, propertyTypes] = await Promise.all([getAmenitiesById(dbClient), getAllPropertyTypes(dbClient)])
+  const [amenities, propertyTypes] = await Promise.all([
+    getAmenitiesById(dbClient),
+    getAllPropertyTypes(dbClient),
+  ])
   if (!amenities.length || !propertyTypes.length) {
     throw new Error("No amenities or property types found in database")
   }
-  const properties = Array.from({ length: NUM_PROPERTIES }, generateProperty)
+  const properties = Array.from({ length: TOTAL_PROPERTIES }, generateProperty)
 
   return await Promise.all(
     properties.map(async property => {
       const assignedPropertyType: PropertyTypeData = randomiseOne(propertyTypes)
-      const assignedAmenities: AmenityData[] = randomiseMany(amenities, { count: NUM_AMENITIES_PER_PROPERTY })
+      const assignedAmenities: AmenityData[] = randomiseMany(amenities, {
+        count: TOTAL_AMENITIES_PER_PROPERTY,
+      })
       const title = `${faker.word.adjective(7)} ${assignedPropertyType.name}`
 
       const [propertyId]: PropertyId[] = await dbClient(Table.Property).insert(
@@ -97,7 +105,8 @@ export const createPropertyTypes = async (dbClient: Knex): Promise<void> => {
   )
 
   const existingPropertyTypes = await dbClient(Table.PropertyType).whereIn("name", PROPERTY_TYPES)
-  if (!existingPropertyTypes.length) await dbClient.insert(propertyTypeData, ["id"]).into(Table.PropertyType)
+  if (!existingPropertyTypes.length)
+    await dbClient.insert(propertyTypeData, ["id"]).into(Table.PropertyType)
 }
 
 export const createAmenities = async (dbClient: Knex): Promise<void> => {
@@ -132,8 +141,7 @@ const updatePropertyPictures = async (
         if (!matchingFile) return
         const [fileName, content] = matchingFile
         if (fileName && content)
-          await uploadToS3(fileName, content, "properties").then(async response => {
-            const result = response
+          await uploadToS3(fileName, content, "properties").then(async () => {
             await replacePrimaryImageForEntity(dbClient, property.id, fileName, Table.Property)
           })
       })
@@ -148,8 +156,13 @@ export async function seed(knex: Knex): Promise<void> {
     await createAmenities(knex)
     await createPropertyTypes(knex)
     const propertyData = await createProperties(knex)
-    await generateImages(propertyData, "properties")
-    await updatePropertyPictures(knex)
+    await generateImages(propertyData, "properties").then(async result => {
+      if (result.isOk()) {
+        await updatePropertyPictures(knex)
+      } else {
+        console.log(result.error)
+      }
+    })
   } catch (error) {
     console.error("Error seeding database:", error)
     throw error
