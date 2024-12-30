415d61e72b3cd0d0d51792257549b0d1aba32c38
anthonymunene <munene.anthony@gmail.com>
2024-12-30 11:45:38 +0000

    feat: (api) refactor error handling

--- apps/api/seeds/users/index.ts
+++ apps/api/seeds/users/index.ts
@@ -1,12 +1,27 @@
 //@ts-ignore
 import { faker } from "@faker-js/faker"
-import { UserAccountDependencies, UserDataGenerator, UserId } from "@seeds/utils/types/users"
-import { Knex } from "knex"
-import { ProfileData, UserProfileDependencies } from "@seeds/utils/types/profiles"
+import {
+  UserAccount,
+  UserAccountDependencies,
+  UserDataGenerator,
+  UserId,
+} from "@seeds/utils/types/users"
+import { ProfilesData, UserProfileDependencies } from "@seeds/utils/types/profiles"
 import { Table } from "@database-generated-types/knex-db"
 import { getFreeProperty, ownProperty } from "@seeds/properties"
-import { TOTAL_USER_ACCOUNTS, USERS_IMAGE_DIR } from "@seeds/utils/variables"
+import {
+  TOTAL_PROPERTIES_TO_OWN,
+  TOTAL_USER_ACCOUNTS,
+  USERS_IMAGE_DIR,
+} from "@seeds/utils/variables"
 import { getMatchingFile, replacePrimaryImageForEntity, uploadToS3 } from "@seeds/utils/shared"
+import { DatabaseClient } from "@seeds/utils/types/shared"
+import { err, ok, ResultAsync } from "neverthrow"
+import { createError } from "@seeds/utils/createError"
+import { DatabaseError, ErrorCode } from "@seeds/utils/types/errors"
+import { Users } from "@services/users/users.schema"
+import { Knex } from "knex"
+import { Profiles } from "@services/profiles/profiles.schema"
 
 export const createDefaultUserDataGenerator: UserDataGenerator = {
   generateUserAccount: () => ({
@@ -47,20 +62,44 @@ export const createUserProfileDependencies = (
   dataGenerator: overrides.dataGenerator || createDefaultUserDataGenerator.generateProfile,
 })
 
-export const createProfiles = async (
+export const createProfiles = (
   user: UserId,
   dependencies: Partial<UserProfileDependencies> = {}
-) => {
+): ResultAsync<ProfilesData, DatabaseError> => {
   const deps = createUserProfileDependencies(dependencies)
   const { dataGenerator, database } = deps
   const { id } = user
-  const profile: Omit<ProfileData, "defaultPic"> = dataGenerator(id)
-  const userProfile = await database.query.insert(profile).into(Table.Profile).returning(["id"])
+  const profile: Omit<ProfilesData, "defaultPic"> = dataGenerator(id)
+  const query: Knex.QueryBuilder<Profiles, ProfilesData[]> = database.query
+    .insert(profile)
+    .into(Table.Profile)
+    .returning("id")
 
-  console.log(`Returned profileId ${JSON.stringify(userProfile[0]?.id)}`)
-  return userProfile[0]
+  return ResultAsync.fromPromise(query, error =>
+    createError(ErrorCode.DATABASE, `something went wrong ${error}`)
+  ).andThen(profile => {
+    if (profile) {
+      console.log(`Returned profileId ${JSON.stringify(profile[0]?.id)}`)
+      ok(profile[0])
+    } else {
+      return err(createError(ErrorCode.DATABASE, `something went wrong creating profile`))
+    }
+  })
 }
 
+export const createAccount = (
+  dbClient: DatabaseClient,
+  userAccount: UserAccount
+): ResultAsync<UserId, DatabaseError> => {
+  const query: Knex.QueryBuilder<Users, UserId[]> = dbClient
+    .insert(userAccount, ["id"])
+    .into(Table.User)
+  return ResultAsync.fromPromise(query, error =>
+    createError(ErrorCode.DATABASE, `could not create user ${error}`)
+  ).andThen(user => {
+    return user?.length ? ok(user[0]) : err(createError(ErrorCode.DATABASE, `something went wrong`))
+  })
+}
 export const createUsersAndProfiles = async (
   dbClient: DatabaseClient,
   dependencies = { createUserAccounts, createProfiles, getFreeProperty, ownProperty }
@@ -75,15 +114,18 @@ export const createUsersAndProfiles = async (
   //   .catch(error => console.log(`something went wrong deleting files:\n${error}`));
 
   for (const userAccount of userAccounts) {
-    const [user]: UserId[] = await knex.insert(userAccount, ["id"]).into(Table.User)
-    if (!user) continue
-    await createProfiles(user, { database: { query: knex } })
+    const user = await createAccount(dbClient, userAccount)
+    if (user.isOk()) {
+      await createProfiles(user.value, { database: { query: dbClient } })
 
       const propertyToOwn = await getFreeProperty(dbClient)
 
-    if (propertyToOwn.length) await ownProperty(propertyToOwn, user, knex)
+      if (propertyToOwn.isOk() && propertyToOwn.value.length) {
+        await ownProperty(propertyToOwn.value, user.value, dbClient, TOTAL_PROPERTIES_TO_OWN)
+      }
 
-    users.push(user)
+      users.push(user.value)
+    }
   }
 
   return users
