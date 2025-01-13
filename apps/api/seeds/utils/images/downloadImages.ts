92a39cf58dbbee06624a4342440c4acedac5b0e1
anthonymunene <munene.anthony@gmail.com>
2025-01-13 22:12:26 +0000

    chore: (api) rename

---
+++ apps/api/seeds/utils/images/downloadImages.ts
@@ -0,0 +1,25 @@
+import { ResultAsync } from "neverthrow"
+import { ErrorCode, ImagesMetaDataError } from "@seeds/utils/types/errors"
+import { createError } from "@seeds/utils/createError"
+import { ImageDownloadResult } from "@seeds/utils/types/images"
+
+export const downloadImages = (
+  links: string[]
+): ResultAsync<ImageDownloadResult[], ImagesMetaDataError> => {
+  //TODO: fetch multiple images here based on the number of urls
+  const imagePromise = links.map(link => {
+    return ResultAsync.fromPromise(fetch(link), error => createError(ErrorCode.NETWORK, `${error}`))
+      .andThen(response =>
+        ResultAsync.fromPromise(response.blob(), error =>
+          createError(ErrorCode.PARSE, `${error}`)
+        ).andThen(blob =>
+          ResultAsync.fromPromise(blob.arrayBuffer(), error =>
+            createError(ErrorCode.PARSE, `${error}`)
+          )
+        )
+      )
+      .map(arrayBuffer => ({ url: link, content: Buffer.from(arrayBuffer) }))
+  })
+
+  return ResultAsync.combine(imagePromise)
+}
