5278000fd5d956fe222e612eb5557632e36fc4bd
anthonymunene <munene.anthony@gmail.com>
2025-01-26 14:28:46 +0000

    chore: (api) adopt renamed types

--- apps/api/seeds/utils/images/extractImageLinks.ts
+++ apps/api/seeds/utils/images/extractImageLinks.ts
@@ -1,22 +1,21 @@
-import { ImagedataResponse } from "@seeds/utils/types/images"
+import { ImageDataWithQueryOptions } from "@seeds/utils/types/images"
 import { err, ok, Result } from "neverthrow"
 import { createError } from "@seeds/utils/createError"
 import { ErrorCode, ImagesMetaDataError } from "@seeds/utils/types/errors"
 
 export const extractImageLinks = (
-  data: ImagedataResponse
+  data: ImageDataWithQueryOptions
 ): Result<string[], ImagesMetaDataError> => {
-  const { options, result } = data
+  const { size, result } = data
   const hasUrls = result.filter(result => {
     return !!result.urls
   })
   if (!hasUrls) {
     return err(createError(ErrorCode.CONFIGURATION, `URL not found`))
   }
-  if (!options?.size) {
+  if (!size) {
     return err(createError(ErrorCode.CONFIGURATION, `Missing size option`))
   }
-  const size = options.size
 
   const images = result.map(results => {
     return results.urls[size]
