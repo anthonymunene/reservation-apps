0ae6038fe44ee9ac3b13ea482e88f92a93b85227
anthonymunene <munene.anthony@gmail.com>
2025-10-18 13:37:16 +0100

    feat: (api) seeding:update image extraction to use metadata and return single URL

--- apps/api/seeds/utils/images/extractImageLinks.ts
+++ apps/api/seeds/utils/images/extractImageLinks.ts
@@ -1,24 +1,20 @@
-import { ImageDataWithQueryOptions } from "@seeds/utils/types/images"
+import { ImageMetadataWithQueryOptions } from "@seeds/utils/types/images"
 import { err, ok, Result } from "neverthrow"
 import { createError } from "@seeds/utils/createError"
 import { ErrorCode, ImagesMetaDataError } from "@seeds/utils/types/errors"
 
 export const extractImageLinks = (
-  data: ImageDataWithQueryOptions
-): Result<string[], ImagesMetaDataError> => {
-  const { size, result } = data
-  const hasUrls = result.filter(result => {
-    return !!result.urls
-  })
-  if (!hasUrls) {
+  data: ImageMetadataWithQueryOptions
+): Result<string, ImagesMetaDataError> => {
+  const { size, urls } = data
+
+  if (!urls) {
     return err(createError(ErrorCode.CONFIGURATION, `URL not found`))
   }
   if (!size) {
     return err(createError(ErrorCode.CONFIGURATION, `Missing size option`))
   }
 
-  const images = result.map(results => {
-    return results.urls[size]
-  })
+  const images = urls[size]
   return ok(images)
 }
