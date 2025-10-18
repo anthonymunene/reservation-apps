81df757e017ea8ef520f7d98413ebc21baf7c005
anthonymunene <munene.anthony@gmail.com>
2025-10-18 13:41:14 +0100

    feat: (api) seeding: simplify function signature

--- apps/api/seeds/utils/images/getImageData.ts
+++ apps/api/seeds/utils/images/getImageData.ts
@@ -1,28 +1,11 @@
-import { ImagedataResponse, ImageResponse } from "@seeds/utils/types/images"
-import { ImagesMetaDataError, ErrorCode } from "@seeds/utils/types/errors"
+import { ImageMetadataWithQueryOptions } from "@seeds/utils/types/images"
+import { ImagesMetaDataError } from "@seeds/utils/types/errors"
 import { getImageMetaData } from "@seeds/utils/images/getImageMetaData"
 import { getImageConfig, imageConfig } from "@seeds/utils/images/getImageConfig"
-import { createError } from "@seeds/utils/createError"
-import { getRandomElement } from "@seeds/utils/getRandomElement"
-import { err, ok, ResultAsync } from "neverthrow"
+import { ResultAsync } from "neverthrow"
 
 export const getImageData = (
-  config: keyof typeof imageConfig,
-  dependencies = { getRandomElement }
-): ResultAsync<ImagedataResponse, ImagesMetaDataError> => {
-  const { getRandomElement } = dependencies
-  return getImageConfig(config)
-    .asyncAndThen(getImageMetaData)
-    .andThen(imageData => {
-      const randomisedResult = getRandomElement<ImageResponse>(imageData.results)
-      if (randomisedResult.isOk()) {
-        const combineWithOptions = {
-          options: imageData.options,
-          result: imageData.results,
-        }
-        return ok(combineWithOptions)
-      } else {
-        return err(createError(ErrorCode.CONFIGURATION, `${randomisedResult.error}`))
-      }
-    })
+  config: keyof typeof imageConfig
+): ResultAsync<ImageMetadataWithQueryOptions, ImagesMetaDataError> => {
+  return getImageConfig(config).asyncAndThen(getImageMetaData)
 }
