7aa2318f04e8043fbc5bbc08300b007d2fe2addf
anthonymunene <munene.anthony@gmail.com>
2025-10-19 15:11:45 +0100

    feat: (api) sanitise: enhance image data sanitisation for 'get' and 'find' methods

--- apps/api/src/hooks/sanitiseImagedata.ts
+++ apps/api/src/hooks/sanitiseImagedata.ts
@@ -4,10 +4,23 @@ import { Properties } from "@services/properties/properties.schema"
 const sanitise = (images: Properties["images"]) => images.map(image => ({ url: image.url }))
 export const sanitiseImageData = async (context: HookContext) => {
   console.log(`Running hook create order on ${context.path}.${context.method}`)
-  context.result.data = context.result.data.map((items: Properties) => {
-    const { images, ...rest } = items
-    const sanitisedImageData = sanitise(images)
-    return { ...rest, images: sanitisedImageData }
-  })
+  if (context.method === "get") {
+    context.result.images = sanitise(context.result.images)
+  }
+  if (context.method === "find") {
+    if (context.result.data) {
+      context.result.data = context.result.data.map((items: Properties) => {
+        const { images, ...rest } = items
+        const sanitisedImageData = sanitise(images)
+        return { ...rest, images: sanitisedImageData }
+      })
+    } else {
+      context.result = context.result.map((items: Properties) => {
+        const { images, ...rest } = items
+        const sanitisedImageData = sanitise(images)
+        return { ...rest, images: sanitisedImageData }
+      })
+    }
+  }
   return context
 }
