265b081306b6fc569317670fc979aff72a98abf6
anthonymunene <munene.anthony@gmail.com>
2025-10-19 15:11:01 +0100

    feat: (api) seeding: streamline seeding process by consolidating error handling and result management

--- apps/api/seeds/01-properties.ts
+++ apps/api/seeds/01-properties.ts
@@ -8,22 +8,14 @@ import {
 import { DatabaseClient } from "@seeds/utils/types/shared"
 
 export async function seed(dbClient: DatabaseClient) {
-  await createAmenities(dbClient)
+  const result = await createAmenities(dbClient)
     .andThen(() => createPropertyTypes(dbClient))
     .andThen(() => createProperties(dbClient))
     .andThen(propertyData =>
-      generateImages(propertyData, "properties")
-        .andThen(() => {
-          return updatePropertyPictures(dbClient)
-        })
-        .map(result => console.log(result))
-        .mapErr(error => {
-          console.log(error)
-          return error
-        })
+      generateImages(propertyData, "properties").andThen(() => updatePropertyPictures(dbClient))
     )
-    .mapErr(error => {
-      console.error("Error during seeding:", error)
-      return error
-    })
+
+  if (result.isErr()) {
+    throw new Error((result.error as Error).message)
+  }
 }
